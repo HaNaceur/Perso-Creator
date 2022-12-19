d85eeeac09e906de11c5ac2b84902a57941894ef
Hamza Naceur <hamzanaceur@MacBook-Air-de-Hamza.local>
2022-12-19 10:13:10 +0100

    feat:install dependancies

---
+++ my-app/src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import CharacterForm from "../CharacterForm/CharacterForm";
+import './App.scss';
+
+function App() {
+
+  return (
+    <div className="App rpgui-content">
+      <CharacterForm/>
+    </div>
+  );
+}
+
+export default App;
